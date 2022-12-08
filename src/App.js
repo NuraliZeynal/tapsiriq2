68020279bc94ce88a938d2b0669ead7b6e1a0548
nuralizeynal <nurelizeynalli@gmail.com>
2022-12-08 12:24:29 +0300

    salam

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 
 import './App.css';
 import Nomre from './Nomre';
+import { Mekteb } from './Mekteb';
 function App() {
+  
 
 
 
@@ -10,8 +12,9 @@ function App() {
     <div className="App">
       <h1>Nurali Zeynalov</h1>
       <h2>Xırdalan şəhərində yaşayıram.</h2>
-      <h3>ADNSU-da təhsil alıram.</h3>
+      
       <Nomre/>
+      <Mekteb/>
       
     </div>
   );
