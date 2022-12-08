b8795a085de72974e246efc21bf38eeef09ff50e
nuralizeynal <nurelizeynalli@gmail.com>
2022-12-08 12:19:51 +0300

    salam

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Nomre from './Nomre';
 function App() {
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Nurali Zeynalov</h1>
+      <h2>Xırdalan şəhərində yaşayıram.</h2>
+      <h3>ADNSU-da təhsil alıram.</h3>
+      <Nomre/>
+      
     </div>
   );
 }
---
+++ src/Nomre.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Nomre = () => {
+  return (
+    <div>051-843-20-03</div>
+  )
+}
+
+export default Nomre
